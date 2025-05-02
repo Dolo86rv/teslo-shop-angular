5e7f88a23508febb5133c505c14816e6808f1b66
Dolo86rv <dolorv86@gmail.com>
2025-05-02 18:23:31 +0200

    Uso de los interceptores y Guards

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -14,7 +14,7 @@ const baseUrl = environment.baseUrl;
 export class AuthService {
   private _authStatus = signal<AuthStatus>('checking');
   private _user = signal<User | null>(null);
-  private _token = signal<string | null>(null);
+  private _token = signal<string | null>(localStorage.getItem('token'));
 
   private http = inject(HttpClient);
 
@@ -56,7 +56,7 @@ export class AuthService {
     return this.http.get<AuthResponse>(`${baseUrl}/auth/check-status`, {
       headers: {
         'Authorization': `Bearer ${token}`
-      }
+      },
     }).pipe(
       map((resp) => this.handleAuthSuccess(resp)),
       catchError((error: any) => this.handleAuthError(error))
@@ -67,7 +67,7 @@ export class AuthService {
     this._authStatus.set('not-authenticated');
     this._user.set(null);
     this._token.set(null);
-    localStorage.removeItem('token');
+    //localStorage.removeItem('token');
   }
 
   private handleAuthSuccess({ token, user}: AuthResponse) {
